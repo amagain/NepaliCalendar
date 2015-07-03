0a04ff04e6374c6a52977a10b07aab49030d0aa6
Bishal Ghimire <bishal.ghimire@gmail.com>
2015-07-03 17:55:05 +0545

    Project Init

---
+++ NepaliCalendar/NepaliCalendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NepaliCalendar
+//
+//  Created by Leapfrog on 7/3/15.
+//  Copyright (c) 2015 Leapfrog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NepaliCalendar/NepaliCalendar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NepaliCalendar
+//
+//  Created by Leapfrog on 7/3/15.
+//  Copyright (c) 2015 Leapfrog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
