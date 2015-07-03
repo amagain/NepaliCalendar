4a7efd56d18d2dce512f031d5afb65521f01faa7
saugat135 <saugatbizid@gmail.com>
2015-07-04 02:36:18 +0545

    Add DateConverterVC classes in folder and project

---
+++ NepaliCalendar/NepaliCalendar/Controller/DateConverterTesterVC/DateConverterTesterVC.h
@@ -0,0 +1,13 @@
+//
+//  DateConverterTesterVC.h
+//  NepaliCalendar
+//
+//  Created by Saugat Gautam on 7/4/15.
+//  Copyright (c) 2015 Leapfrog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DateConverterTesterVC : UIViewController
+
+@end
---
+++ NepaliCalendar/NepaliCalendar/Controller/DateConverterTesterVC/DateConverterTesterVC.m
@@ -0,0 +1,37 @@
+//
+//  DateConverterTesterVC.m
+//  NepaliCalendar
+//
+//  Created by Saugat Gautam on 7/4/15.
+//  Copyright (c) 2015 Leapfrog. All rights reserved.
+//
+
+#import "DateConverterTesterVC.h"
+
+@interface DateConverterTesterVC ()
+
+@end
+
+@implementation DateConverterTesterVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
